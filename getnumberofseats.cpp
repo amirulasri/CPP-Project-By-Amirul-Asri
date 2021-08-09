809edc8b2d265c835ed33e63971542861508e5df
amirulasri <amirulasrix@gmail.com>
2021-08-09 18:00:52 +0800

    New file

---
+++ getnumberofseats.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int biloffamilies, seater;
+
+    cout<<"Enter family number: ";
+    cin>>biloffamilies;
+    seater = biloffamilies * 3;
+
+    cout<<"\nSeater needed is: "<<seater;
+
+    return 0;
+}
