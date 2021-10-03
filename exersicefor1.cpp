3b32dc391c1eed5c18e9b28faebd36e7e3a5a285
amirulasri <amirulasrix@gmail.com>
2021-10-03 23:29:32 +0800

    Update

--- exersicefor1.cpp
+++ exersicefor1.cpp
@@ -5,16 +5,6 @@ int main()
 {
     int nombor = 100;
 
-    //DO WHILE
-    do{
-        if ((nombor % 2) != 0)
-        {
-            cout << nombor << endl;
-        }
-
-        nombor++;
-    }while (nombor <= 200);
-
     //WHILE
     while (nombor <= 200){
         if ((nombor % 2) != 0)
