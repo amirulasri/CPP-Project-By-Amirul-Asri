6b0a39470ffa6ce6f4d0a787adbf12f1eddf23ea
amirulasri <amirulasrix@gmail.com>
2022-02-19 16:15:15 +0800

    Update

---
+++ PART 2/javahotelappstarter.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hotel Sri Kayangan GUI JAVA App by Amirul Asri\nCopyright 2022\n\n";
+    system("java -jar \"hotelsystembyamirulasri.jar\"");
+}
