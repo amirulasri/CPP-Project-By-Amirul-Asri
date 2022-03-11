b444352fa1965dcc8536ee8e26e1715898f4d3ba
amirulasri <amirulasrix@gmail.com>
2022-03-11 12:06:33 +0800

    Update

---
+++ PART3/sablatihan24.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+
+using namespace std;
+
+int nomTempatan()
+{
+   int nom;
+   cout << "Masukkan Nombor Tempatan:";
+   cin >> nom;
+   return nom;
+}
+
+int nomAntarabangsa()
+{
+   int nom;
+   cout << "Masukkan Nombor Antarabangsa:";
+   cin >> nom;
+   return nom;
+}
+
+void mobilekemobile(int nomT)
+{
+   int caj, cukai, total;
+   if (nomT == 100)
+   {
+      caj = nomT * 0.50;
+      cukai = nomT * 0.08;
+      total = caj + cukai;
+      cout << "Mobile ke mobile" << endl;
+      cout << "jumlah bil telefon tempatan: RM " << total << endl
+           << endl;
+   }
+   else if (nomT > 100 && nomT <= 200)
+   {
+      caj = nomT * 0.45;
+      cukai = nomT * 0.07;
+      total = caj + cukai;
+      cout << "Mobile ke mobile" << endl;
+      cout << "jumlah bil telefon tempatan: RM " << total << endl
+           << endl;
+   }
+   else if (nomT > 200 && nomT <= 500)
+   {
+      caj = nomT * 0.40;
+      cukai = nomT * 0.06;
+      total = caj + cukai;
+      cout << "Mobile ke mobile" << endl;
+      cout << "jumlah bil telefon tempatan: RM " << total << endl
+           << endl;
+   }
+   else if (nomT > 500)
+   {
+      caj = nomT * 0.30;
+      cukai = nomT * 0.05;
+      total = caj + cukai;
+      cout << "Mobile ke mobile" << endl;
+      cout << "jumlah bil telefon tempatan:RM " << total << endl
+           << endl;
+   }
+}
+
+void mobileketalian(int nomT)
+{
+   int caj, cukai, total;
+   if (nomT == 100)
+   {
+      caj = nomT * 0.30;
+      cukai = nomT * 0.03;
+      total = caj + cukai;
+      cout << "Mobile ke talian tetap" << endl;
+      cout << "jumlah bil telefon tempatan :RM" << total << endl
+           << endl;
+   }
+   else if (nomT > 100 && nomT <= 200)
+   {
+      caj = nomT * 0.25;
+      cukai = nomT * 0.02;
+      total = caj + cukai;
+      cout << "Mobile ke talian tetap" << endl;
+      cout << "jumlah bil telefon tempatan:RM" << total << endl
+           << endl;
+   }
+   else if (nomT > 200 && nomT <= 500)
+   {
+      caj = nomT * 0.20;
+      cukai = nomT * 0.01;
+      total = caj + cukai;
+      cout << "Mobile ke talian tetap" << endl;
+      cout << "jumlah bil telefon tempatan: RM" << total << endl
+           << endl;
+   }
+   else if (nomT > 500)
+   {
+      caj = nomT * 0.10;
+      cukai = nomT * 0.0;
+      total = caj + cukai;
+      cout << "Mobile ke talian tetap" << endl;
+      cout << "jumlah bil telefon tempatan: RM" << total << endl
+           << endl;
+   }
+}
+
+int semuajenispanggilan(int nomA)
+{
+   int caj, cukai, total;
+   if (nomA == 100)
+   {
+      caj = nomA * 0.60;
+      cukai = nomA * 0.10;
+      total = caj + cukai;
+      cout << "Semua jenis panggilan" << endl;
+      cout << "jumlah bil telefon Antarabangsa: RM" << total << endl
+           << endl;
+   }
+   else if (nomA > 100 && nomA <= 200)
+   {
+      caj = nomA * 0.55;
+      cukai = nomA * 0.08;
+      total = caj + cukai;
+      cout << "Semua jenis panggilan" << endl;
+      cout << "jumlah bil telefon Antarabangsa:RM " << total << endl
+           << endl;
+   }
+   else if (nomA > 200 && nomA <= 500)
+   {
+      caj = nomA * 0.50;
+      cukai = nomA * 0.06;
+      total = caj + cukai;
+      cout << "Semua jenis panggilan" << endl;
+      cout << "jumlah bil telefon Antarabangsa:RM" << total << endl
+           << endl;
+   }
+   else if (nomA > 500)
+   {
+      caj = nomA * 0.40;
+      cukai = nomA * 0.0;
+      total = caj + cukai;
+      cout << "Semua jenis panggilan" << endl;
+      cout << "jumlah bil telefon Antarabangsa:RM" << total << endl
+           << endl;
+   }
+}
+
+int main()
+{
+   int nomT[5], nomA[5], mobile, talian, panggilanA;
+   int bilpengguna;
+
+   for (int i = 0; i < 5; i++)
+   {
+      nomT[i] = nomTempatan();
+      nomA[i] = nomAntarabangsa();
+   }
+   cout << "\n\n";
+   for (int i = 0; i < 5; i++)
+   {
+      mobilekemobile(nomT[i]);
+      mobileketalian(nomT[i]);
+      panggilanA = semuajenispanggilan(nomA[i]);
+   }
+}
---
+++ PART3/sablatihan25.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string nama[3] = {"Sabrina", "Siti", "Ali"};
+    
+    for (int i = 0; i < 3; i++)
+    {
+        cout << nama[i] << endl;
+    }
+    
+}
