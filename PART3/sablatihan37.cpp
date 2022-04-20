e98eacef87adca065af5a7de2b03d63001ff7ed3
amirulasri <amirulasrix@gmail.com>
2022-04-20 18:42:47 +0800

    Update

--- PART3/sablatihan37.cpp
+++ PART3/sablatihan37.cpp
@@ -6,10 +6,9 @@ const int baris=3;
 const int lajur=2;
 
 int main()
-{
-    int saiz[baris][lajur]={{20,57},{40,30},{35,37}};
-                            
-    
+{                           //   0           1           2
+    int saiz[baris][lajur]={  {20,57}  ,  {40,30}  ,  {35,37}  };               
+                            // 00 01       10 11       20 21
     for(int i=0; i<lajur;i++)
     {
         for(int j=0; j<baris; j++)
---
+++ PART3/sablatihan41.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    for (int i = 0; i < 2; i++)
+    {
+        cout << "Ulangan i: " << i << endl;
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "Ulangan j: " << j << endl;
+        }
+        
+    }
+    
+}
